5ffb4fc93082fa956c8c0a10ec5f4c6bb44ad2ca
Sue-cod <snozic892@gmail.com>
2021-03-29 10:01:23 +0200

    vikend pr

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -3,3 +3,9 @@ function myFunction(rock) {
   document.getElementById("area").innerHTML += rock;
   console.log(rock);
 }
+
+$(window).keypress(function(e) {
+  if (e.keyCode == 0 || e.keyCode == 32) {
+    console.log('Space pressed');
+  }
+});
