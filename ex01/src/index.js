4c18a887e7d7bc6a7bd8db44329e493a41083a19
Sue-cod <snozic892@gmail.com>
2021-03-29 10:19:44 +0200

    ok

--- ex01/src/index.js
+++ ex01/src/index.js
@@ -9,3 +9,20 @@ $(window).keypress(function(e) {
     console.log('Space pressed');
   }
 });
+
+function targetPopup(form) {
+  window.open('', 'formpopup', 'width=400,height=400,resizeable,scrollbars');
+  form.target = 'formpopup';
+}
+
+function submit()
+{
+    var name = document.getElementById("name").value;
+    var output = " My Name is "+name+" ";
+    var myWindow = window.open("data:text/html," + encodeURIComponent(output),
+               "_blank", "width=200,height=100");
+
+    x = window.print();
+
+    return false;
+}
